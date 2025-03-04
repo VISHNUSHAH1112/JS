01482681eecf409fe7bdac635090e0e959657b21
VIshnu Shah <shahvishnu1112@gmail.com>
2025-03-04 19:12:34 +0530

    Add files via upload

---
+++ Array 1/even.js
@@ -0,0 +1,12 @@
+// wap to print only even numbers from Array.
+
+let a=[1,2,3,4,5,6,7,8,9,10]
+for(i=0; i<a.length; i++)
+{
+    if(i%2==0)
+        {
+        document.write("Given Number Is Even" + "  ")
+        // document.write("<br>")
+        document.write([i] + " ")
+    }
+}
---
+++ Array 1/oddeven.js
@@ -0,0 +1,12 @@
+let a=[1,2,3,4,5,6,7,8,9,10]
+
+for(let i=0; i<a.length; i++)
+{
+    if(i%2==0)
+    {
+        document.write([0] + "  ")
+    }
+    else{
+        document.write([1]+ "  ");
+    }
+}
